63db67c7c156a8ddad4d10b758ddf23bdd91e0da
Florian Jacob Breitwieser <flo2223@gmx.com>
2026-04-26 11:58:18 +0200

    Rx improved

--- A4_Abgabe/code/rtl/uart_rx.vhd
+++ A4_Abgabe/code/rtl/uart_rx.vhd
@@ -13,54 +13,62 @@ entity uart_rx is
 architecture uart_rx_arch of uart_rx is
 
     signal cache : std_logic_vector(7 downto 0) := (others => '0');
-    signal uart_sync : std_logic_vector(2 downto 0) := (others => '0');
+    uart_sync : std_logic_vector(1 downto 0) := (others => '0');
     constant baud_rate : integer := 9600;
     constant clk_freq : integer := 12000000;
-    constant bit_period : integer := clk_freq / baud_rate;
+    constant bit_period : integer := clk_freq / baud_rate - 1;
     type state_type is (IDLE, READING);
     signal state : state_type := IDLE;
 
 begin
 
-    process(uart_in)
-    begin
-        if falling_edge(uart_in) and state = IDLE then
-            state <= READING;
-            bit_cnt := 0;
-            clk_cnt := 0;
-            cache <= (others => '0');
-        end if;
-    end process;
-
-
     process(clk)
             variable pre_cnt : integer range 0 to bit_period / 2 := 0;
             variable post_cnt : integer range 0 to bit_period / 2 := 0;
-            variable bit_cnt : integer range 0 to 8 := 0;
-            variable clk_cnt : integer range 0 to bit_period * 11 - 1 := 0;
+            variable bit_cnt : integer range 0 to 7 := 0;
+            variable clk_cnt : integer range 0 to bit_period := 0;
 
     begin
         if rising_edge(clk) then
+
+            uart_sync <= uart_sync(1 downto 0) & uart_in; -- Shift in the new bit for synchronization
+
             if state = IDLE then
 
-                uart_sync <= uart_sync(1 downto 0) & uart_in; -- Shift in the new bit for synchronization
-                if uart_sync(2 downto 1) = "10" then -- Detect the falling edge for start bit
+                if uart_sync(1 downto 0) = "10" then -- Detect the falling edge for start bit
                     state <= READING;
+                    pre_cnt := 0;
+                    post_cnt := 0;
                     bit_cnt := 0;
                     clk_cnt := 0;
                     cache <= (others => '0');
                 end if;
 
-            else
+            elsif state = READING then
 
-                if clk_cnt = ((bit_cnt + 1) * bit_period) + (bit_period / 2 )  then
+                if pre_cnt < (bit_period / 2) then
 
-                    if bit_cnt < 8 then
-                        cache(bit_cnt) <= uart_in;
-                        bit_cnt <= bit_cnt + 1;
+                    pre_cnt := pre_cnt + 1;
+
+                elsif bit_cnt < 7 then
+                    
+                    if clk_cnt < bit_period then
+
+                        clk_cnt <= clk_cnt + 1;
+
+                    else
+
+                        cache(bit_cnt) <= uart_sync(0); -- Sample the bit
+                        bit_cnt := bit_cnt + 1;
+                        clk_cnt := 0;
+
                     end if;
 
-                elsif clk_cnt = clk_cnt'high then
+                elsif post_cnt < (bit_period * 1.5) then
+                
+                    post_cnt := post_cnt + 1;
+
+                else
 
                     if unsigned(cache) = 106 then  --checks if the received byte is 106 (ASCII for 'j')
                         led <= '1';
@@ -68,11 +76,7 @@ begin
                         led <= '0';
                     end if;
                     state <= IDLE;
-
-                else 
-
-                    clk_cnt <= clk_cnt + 1;
-
+                    
                 end if;
             end if;
         end if;
