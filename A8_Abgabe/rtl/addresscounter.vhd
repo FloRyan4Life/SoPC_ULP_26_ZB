097dfe92abce57b94cf80a90e512ffe6c93b521b
Florian Jacob Breitwieser <flo2223@gmx.com>
2026-06-10 01:50:56 +0200

    rebranch

---
+++ A8_Abgabe/rtl/addresscounter.vhd
@@ -0,0 +1,40 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity addresscounter is
+    port(
+        clk : in std_logic;
+        acnt_rst : in std_logic;
+        acnt_inc : in std_logic;
+        acnt_eq191  : out std_logic;
+        address : out std_logic_vector(7 downto 0)
+    );
+end addresscounter;
+
+architecture addresscounter_a of addresscounter is
+
+
+begin
+    
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if acnt_rst ='1' then
+
+                address <= (others => '0');
+
+            elsif acnt_inc = '1' then
+
+                address <= std_logic_vector(unsigned(address) + 1);
+                if unsigned(address) = 191 then
+                    acnt_eq191 <= '1';
+                else
+                    acnt_eq191 <= '0';
+                end if;
+
+            end if;
+        end if;
+    end process;
+
+end architecture addresscounter_a;
---
+++ A8_Abgabe/rtl/shiftregister.vhd
@@ -0,0 +1,53 @@
+libary ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity shiftregister is
+    port(
+        clk : in std_logic;
+        sfr_din : in std_logic_vector(7 downto 0);
+        sfr_rst : in std_logic;
+        sfr_shift : in std_logic;
+        sfr_load : in std_logic;
+        sfr_serout : out std_logic;
+        sfr_done : out std_logic
+    );
+end shiftregister;
+
+architecture shiftregister_a of shiftregister is
+    
+    signal shift_reg : std_logic_vector(7 downto 0) := (others => '0');
+
+begin
+
+    process(clk)
+        variable shift_cnt : integer range 0 to 7 := 0;
+    begin
+        if rising_edge(clk) then
+            -- sfr_serout <= shift_reg(7);
+            -- Im process oder nicht?
+            -- Was macht mehr Sinn? 
+            if sfr_rst = '1' then
+                shift_reg <= (others => '0');
+                shift_cnt := 0;
+                sfr_done <= '0';
+
+            elsif sfr_load = '1' then
+                shift_reg <= sfr_din;
+                shift_cnt := 0;
+                sfr_done <= '0';
+
+            elsif sfr_shift = '1' then
+                shift_reg <= shift_reg(6 downto 0) & '0';
+                shift_cnt := shift_cnt + 1;
+                if shift_cnt = 7 then
+                    sfr_done <= '1';
+                end if;
+
+            end if;
+        end if;
+    end process;
+
+    sfr_serout <= shift_reg(7);
+
+end shiftregister_a;
