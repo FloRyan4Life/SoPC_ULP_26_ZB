ec3495468fc5e8361aa7bee4e5246b34c048b29f
jzimme14 <jakob.zimmermann@smail.th-koeln.de>
2026-06-11 10:58:31 +0200

    added pcf and stuff

--- A8_Abgabe/rtl/RAMtoWS2812.vhd
+++ A8_Abgabe/rtl/RAMtoWS2812.vhd
@@ -2,8 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library work;
-use work.const_image.all;
+-- library work;
+-- use work.const_image.all;
 
 entity RAMtoWS2812 is
     port(
